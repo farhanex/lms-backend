dbca9391426ccb398fef6d8df23d46976ef72cb6
Farhan3439A <112370960+Farhan3439A@users.noreply.github.com>
2024-07-14 16:54:09 +0530

    final commit

--- services/emailService.js
+++ services/emailService.js
@@ -1,36 +1,76 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 // const EMAIL_USER = process.env.EMAIL_USER;
 // const EMAIL_PASS = process.env.EMAIL_PASS;
 
+// // config
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   service: 'gmail',
+//   auth: {
+//     user: EMAIL_USER,
+//     pass: EMAIL_PASS,
+//   },
+// });
+
+// const sendResetEmail = (email, token) => {
+//   const resetUrl = `http://localhost:5173/reset-password/${token}`;
+//   const mailOptions = {
+//     from: process.env.EMAIL_USER,
+//     to: email,
+//     subject: 'Password Reset',
+//     html: `<p> You requested for password reset . Click on the link to reset your password: <a href="${resetUrl}">${resetUrl}</a></p>`,
+//   };
+//   console.log(EMAIL_USER,EMAIL_PASS,email);
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//     }
+//   });
+// };
+
+// module.exports = sendResetEmail;
+
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const EMAIL_USER = process.env.EMAIL_USER;
+const EMAIL_PASS = process.env.EMAIL_PASS;
+
 // config
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: EMAIL_USER,
+    pass: EMAIL_PASS,
   },
 });
 
 const sendResetEmail = (email, token) => {
-  const resetUrl = `https://lms-frontend-blond.vercel.app/reset-password/${token}`;
+  const resetUrl = `http://localhost:5173/reset-password/${token}`;
   const mailOptions = {
-    from: process.env.EMAIL_USER,
+    from: EMAIL_USER,
     to: email,
     subject: 'Password Reset',
-    html: `<p> You requested for password reset . Click on the link to reset your password: <a href="${resetUrl}">${resetUrl}</a></p>`,
+    html: `<p>You requested a password reset. Click on the link to reset your password: <a href="${resetUrl}">${resetUrl}</a></p>`,
   };
-  // console.log(EMAIL_USER,EMAIL_PASS,email);
+  console.log('EMAIL_USER:', EMAIL_USER);
+  console.log('EMAIL_PASS:', EMAIL_PASS);
+  console.log('Recipient Email:', email);
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log(error);
+      console.log('Error:', error);
     } else {
-      // console.log('Email sent: ' + info.response);
+      console.log('Email sent: ' + info.response);
     }
   });
 };
 
 module.exports = sendResetEmail;
+
